284ce730dd764a591ea984c3dc25c85377792a7e
yohann <yohann.durel@gmail.com>
2022-01-02 21:12:25 +0100

    cleanup code

--- src/fr/epsi/b3/recensement/Application.java
+++ src/fr/epsi/b3/recensement/Application.java
@@ -10,51 +10,14 @@ public class Application {
 
         String file = "../recensement_2016.csv";
         String line;
-        List<String[]> tableaucsv = new ArrayList<>();
         String [] ligne;
         BufferedReader br = new BufferedReader(new FileReader(file));
         br.readLine();//Lis la première ligne, n'en fait rien
         while((line = br.readLine()) != null){
             ligne = line.split(";");
-            tableaucsv.add(ligne);
             recensement.addVille(Integer.parseInt(ligne[0]), ligne[1], ligne[2], Integer.parseInt(ligne[5]), ligne[6], Integer.parseInt(ligne[9].replaceAll("\s", "")));
         }
 
-
-        HashMap<String, Integer> compteurs = new HashMap<>();
-
-        for (int i = 0; i < recensement.getVilles().size(); i++) {
-            Ville ville = recensement.getVilles().get(i);
-            String nomCommune = ville.getNomCommune();
-
-            Integer compteur = compteurs.get(nomCommune);
-            if (compteur == null) {
-                compteur = 0;
-            }
-            compteur++;
-            compteurs.put(nomCommune, compteur);
-        }
-
-        Set<String> nomCommunes = compteurs.keySet();
-        Iterator<String> iterateur = nomCommunes.iterator();
-
-        while (iterateur.hasNext()) {
-            String nomCommune = iterateur.next();
-            Integer valeurCompteur = compteurs.get(nomCommune);
-            System.out.println("Nom commune : " + nomCommune + " - Nombre de région : " + valeurCompteur);
-        }
-
-//        String[] morceaux = ligne.split(";");
-//        String codeRegion = morceaux[0];
-//        String nomRegion = morceaux[1];
-//        String codeDepartement = morceaux[2];
-//        String codeCommune = morceaux[5];
-//        String nomCommune = morceaux[6];
-//        String population = morceaux[7];
-//
-//        int populationTotale = Integer.parseInt(population.replace(" ", "").trim());
-//
-//        Ville ville = new Ville(codeRegion, nomRegion, codeDepartement, codeCommune, nomCommune, populationTotale);
     }
 
 
